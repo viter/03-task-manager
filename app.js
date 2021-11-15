37e55398a125f1c3a104aa4926f591c92e5445a3
viter <chekhiv@gmail.com>
2021-11-15 16:10:36 +0200

    new commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 const app = express();
 
@@ -10,11 +11,19 @@ app.get('/hello', (req, res) => {
   res.send('Task Manager App');
 });
 
-
 app.use('/api/v1/tasks', tasks);
 
 const port = process.env.PORT | 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.DB_CONNECTION_STRING);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,27 +1,30 @@
+const Task = require('../models/Task');
+
 const getAllTasks = (req, res) => {
   res.send('get all tasks');
-}
+};
 
-const createTask = (req, res) => {
-  res.send('create new task');
-}
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+};
 
 const getTask = (req, res) => {
-  res.send('get single task');
-}
+  res.json({ id: req.params.id });
+};
 
 const updateTask = (req, res) => {
   res.send('update task');
-}
+};
 
 const deleteTask = (req, res) => {
   res.send('delete task');
-}
+};
 
 module.exports = {
   getAllTasks,
   createTask,
   getTask,
   updateTask,
-  deleteTask
-}
+  deleteTask,
+};
