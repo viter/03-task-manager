b16d6ea715a2fbaacdd6065d7a7e4a3b36ecb7bd
viter <chekhiv@gmail.com>
2021-11-16 12:41:51 +0200

    lesson 113

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,31 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-  res.send('get all tasks');
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ id: taskID });
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
