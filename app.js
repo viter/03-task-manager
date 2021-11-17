44c535f8c0981c97260469d70ee39882bd40d046
virer <chekhiv@gmail.com>
2021-11-17 18:50:59 +0200

    lesson 121

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 
 const port = process.env.PORT | 3000;
 
