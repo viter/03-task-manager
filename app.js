8a61967063898c74ec2e591427ff3016e3532e52
viter <chekhiv@gmail.com>
2021-11-17 15:23:01 +0200

    Lesson 120

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
+require("dotenv").config();
+const express = require("express");
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 const app = express();
 
+app.use(express.static("./public"));
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App');
-});
-
-app.use('/api/v1/tasks', tasks);
+app.use("/api/v1/tasks", tasks);
 
 const port = process.env.PORT | 3000;
 
