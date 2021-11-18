9773cd20402f6057d53065500c0dcb700e352b5f
viter <chekhiv@gmail.com>
2021-11-18 13:06:47 +0200

    lesson 122

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const app = express();
 
---
+++ middleware/error-handler.js
@@ -0,0 +1,3 @@
+const errorHandlerMiddleware = (err, req, res, next) => {
+  return res.status(500).json({ msg: err });
+};
